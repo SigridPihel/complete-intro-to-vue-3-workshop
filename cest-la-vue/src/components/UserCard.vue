b52611f89888782ff17f65f9ab64d996d4ff145b
sigrid.pihel <sigrid.pihel@iute.com>
2026-02-10 09:11:28 +0200

    updated

---
+++ cest-la-vue/src/components/UserCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+defineProps( {
+    user: {
+        type: Object,
+        required: true,
+    }
+})
+
+</script>
+
+<template>
+    <li class="user-card">
+        {{ user.name }}: {{ user.website }}
+    </li>
+</template>
+
+<style>
+.user-card {
+    border: 2px solid #222;
+    padding: 10px;
+}
+</style>
---
+++ cest-la-vue/src/components/UserPage.vue
@@ -0,0 +1,73 @@
+<script setup>
+import UserCard from './UserCard.vue';
+import { defineProps, reactive } from 'vue';
+
+defineProps({
+  title: {
+    type: String,
+    default: 'Users'
+  }
+})
+
+defineEmits(['update-user-list'])
+
+const state = reactive({
+  userList: [],
+});
+
+async function fetchUsers() {
+  const response = await fetch(
+      "https://jsonplaceholder.typicode.com/users"
+  ).then((response) => response.json());
+
+  return response;
+}
+
+state.userList = await fetchUsers();
+
+</script>
+
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+    <ul>
+        <UserCard 
+            v-for="user in state.userList" 
+            :user="user" 
+            :key="`user-${user.id}`" 
+        />
+    </ul>
+  </main>
+</template>
+
+<style>
+main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  max-width: 320px;
+  margin: 0 auto;
+}
+
+main h1 {
+  margin-top: 10vh;
+  margin-bottom: 20px;
+}
+
+label {
+  margin-bottom: 5px;
+}
+
+input[type="email"] {
+  padding: 0.5rem;
+  margin-bottom: 30px;
+}
+
+button {
+  border: 1px solid green;
+  padding: 10px;
+  color: green;
+  background-color: rgb(213, 255, 213);
+  cursor: pointer;
+}
+</style>
